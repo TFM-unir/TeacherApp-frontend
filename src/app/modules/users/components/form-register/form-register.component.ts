6eb7d27f402d08603b8515af680d804a0275b2f0
mairi tikk <mairi.tikk@gmail.com>
2023-11-30 23:40:33 +0100

    continuando con formregister

--- src/app/modules/users/components/form-register/form-register.component.ts
+++ src/app/modules/users/components/form-register/form-register.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-form-register',
   templateUrl: './form-register.component.html',
-  styleUrls: ['./form-register.component.css']
+  styleUrls: ['./form-register.component.css'],
 })
 export class FormRegisterComponent {
+  private usersService = inject(UsersService);
+  //private router =
 
+  async ngOnInit() {
+    try {
+      const user = `{
+        userForm: {
+          name: 'Juana Alvarez',
+          nickname: 'juasdn123',
+          email: 'pedesdasd@gmail.com',
+          phone: '643434548',
+          password: '12345',
+          date_of_birth: '1993-02-15',
+          status: 2,
+          role_id: 2,
+          photo: 'url',
+        },
+        teacherForm: {
+          experience: 5,
+          class_mode: 'Mañana',
+          price_hour: 10.5,
+          about_me: 'Soy un soñador brutal con todos los hierros',
+        },
+        locationForm: {
+          latitude: 41.385063,
+          longitude: 2.987456,
+          address: 'calle de quintal 25',
+          city: 'Santiago',
+          province: 'A Coruña',
+        },
+      };`;
+
+      const response = await this.usersService.Register(user);
+      console.log(response);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
 }
--- src/app/modules/users/services/users.service.ts
+++ src/app/modules/users/services/users.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  private baseUrlRegister: string = 'http://localhost:3000/api/users/register';
+  private baseUrlStudents: string = 'http://localhost:3000/api/departments';
+  private httpClient = inject(HttpClient);
 
-  constructor() { }
+  Register(user: string) {
+    return lastValueFrom(this.httpClient.post(this.baseUrlRegister, user));
+  }
+
+  getAllStudents() {
+    return firstValueFrom(this.httpClient.get(this.baseUrlStudents));
+  }
 }
