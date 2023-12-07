865036c855f00ef3a23f87396d1d220b037941ee
miquelmb <miquelmayordomo@gmail.com>
2023-12-07 19:34:40 +0100

    Avances en mapa home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,11 +5,12 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { HeroSectionComponent } from './components/home/home-components/hero-section/hero-section.component';
 import { MapSectionComponent } from './components/home/home-components/map-section/map-section.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [HomeComponent, HeroSectionComponent, MapSectionComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
   exports: [HomeComponent, HeroSectionComponent, MapSectionComponent]
 })
 export class HomeModule { }
---
+++ src/app/shared/components/map/map.component.ts
@@ -0,0 +1,70 @@
+import { Component, ViewChild } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { TeacherProfile } from 'src/app/core/models/teacher.interface';
+import { TeachersService } from 'src/app/modules/teachers/services/teachers.service';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+
+export class MapComponent {
+
+  teachers: TeacherProfile[] | undefined;
+
+  @ViewChild(MapInfoWindow) miInfoWindow: MapInfoWindow | any;
+
+  zoom: number = 12;
+  center: google.maps.LatLng = new google.maps.LatLng(40.41831, -3.70275);
+  myposition: google.maps.LatLng | any = new google.maps.LatLng(40.41831, -3.70275);
+  markerOptionsUser = {
+    animation: google.maps.Animation.BOUNCE,
+    icon: {
+      url: '../../../../assets/userPosition.svg',
+      scaledSize: new google.maps.Size(40, 40),
+    },
+  };
+  markerOptionsTeachers = {
+    animation: google.maps.Animation.BOUNCE,
+    icon: {
+      url: '../../../../assets/teacherPosition.svg',
+      scaledSize: new google.maps.Size(40, 40),
+    },
+  };
+
+  // revisar xk no funciona?
+  mapOptions: google.maps.MapOptions = {
+    streetViewControl: false, // Deshabilitar control de street view
+  };
+
+  constructor(private teachersService: TeachersService) {}
+
+  ngOnInit() {
+    this.getAllTeachers();
+    this.initiateGeolocation();
+  }
+
+  initiateGeolocation(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      //console.log(position);
+      const { latitude, longitude } = position.coords;
+      this.myposition = new google.maps.LatLng(latitude, longitude);
+      this.center = this.myposition;
+    });
+  }
+
+  openInfoWindow(miMarker: MapMarker) {
+    this.miInfoWindow.open(miMarker);
+  }
+
+  async getAllTeachers(): Promise<void> {
+    try {
+      this.teachers = await this.teachersService.getAllTeachers();
+    } catch (error) {
+      console.error('Error fetching teachers:', error);
+    }
+    console.log(this.teachers)
+  }
+
+}
