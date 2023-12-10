7e87a15085dafcf4feef9f24053814205e20a562
Ruben Hernandez <rubenh9322@gmail.com>
2023-12-10 17:40:07 +0100

    Ultimo push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,18 @@ import { AuthService } from './core/services/auth.service';
 export class AppComponent {
   title = 'TeacherApp-frontend';
 
-  //Inyectamos auth service
-  authService = inject(AuthService);
+  //Inyectamos el servicio del core
+  private coreService = inject(AuthService);
+  //Declaramos la variable users que contendra todos los datos del usuario
+  user: User | any;
+  //Decalramos booleana variable que permita que se pinte de color si esteacher o student
+  booleanPaint: boolean = false
 
   ngOnInit() {
-    console.log(this.authService.getUserRoleName());
+    this.user = this.coreService.getDecodedToken();
+    if (this.user.user_role === 2) {
+      this.booleanPaint = true;
+    }
+
   }
 }
