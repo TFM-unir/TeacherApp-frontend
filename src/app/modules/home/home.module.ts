e5f668e820d626c4db093e84db4e89f31d36db6c
miquelmb <miquelmayordomo@gmail.com>
2023-12-10 01:24:22 +0100

    Home adaptado a requerimentos tfm (orden x puntuación). Card mejorada

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,10 +6,10 @@ import { HomeComponent } from './components/home/home.component';
 import { HeroSectionComponent } from './components/home/home-components/hero-section/hero-section.component';
 import { MapSectionComponent } from './components/home/home-components/map-section/map-section.component';
 import { GoogleMapsModule } from '@angular/google-maps';
-
+import { JoinTeacherAppComponent } from './components/home/home-components/join-teacher-app/join-teacher-app.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeroSectionComponent, MapSectionComponent],
+  declarations: [HomeComponent, HeroSectionComponent, MapSectionComponent, JoinTeacherAppComponent],
   imports: [CommonModule, HomeRoutingModule, GoogleMapsModule],
   exports: [HomeComponent, HeroSectionComponent, MapSectionComponent]
 })
