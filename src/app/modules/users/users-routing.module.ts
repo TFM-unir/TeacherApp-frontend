89973f8ddbfd554738085eb211f9c48ae7fcca71
mairi tikk <mairi.tikk@gmail.com>
2023-12-10 11:30:11 +0100

    Eliminando mapa que no se necesita

--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormRegisterComponent } from './components/form-register/form-register.component';
 import { FormLoginComponent } from './components/form-login/form-login.component';
-import { MapaComponent } from './components/mapa/mapa.component';
 
 const routes: Routes = [
   { path: 'register', component: FormRegisterComponent },
   { path: 'login', component: FormLoginComponent },
-  { path: 'mapa', component: MapaComponent },
 ];
 
 @NgModule({
